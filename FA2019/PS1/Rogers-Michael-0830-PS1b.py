7ae4d43338508b3522c1ef5e86c4df7e56a7cc43
Michael <mike.rogers1272@gmail.com>
2019-09-02 10:33:33 -0600

    PS1b changes

--- FA2019/PS1/Rogers-Michael-0830-PS1b.py
+++ FA2019/PS1/Rogers-Michael-0830-PS1b.py
@@ -3,7 +3,6 @@ import random
 def count_flip(n):
     print("Create random array of size " + str(n))
     rand_arr = [x for x in range(1,n + 1)] # Create list of numbers between 1 and n
-    # rand_arr = [1,3,5,2,4,6]
     random.shuffle(rand_arr) # Shuffle randomly generated list of numbers
     count = 0 # Initialize the counter at 0
     for i in range(n):
@@ -12,8 +11,12 @@ def count_flip(n):
                 count += 1
     return count
 
+# print(count_flip(2**16))
 
 
-
-
-print(count_flip(2**16))
+def swap_flip(arr):
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] > arr[j] and i == (j - 1):
+                swap(i, j)
+            

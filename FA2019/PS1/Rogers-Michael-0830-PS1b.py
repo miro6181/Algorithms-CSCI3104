961d669305daaa69dda63c0c4cebcdc436e72922
Michael <mike.rogers1272@gmail.com>
2019-09-02 10:32:22 -0600

    PS1b

---
+++ FA2019/PS1/Rogers-Michael-0830-PS1b.py
@@ -0,0 +1,19 @@
+import random
+
+def count_flip(n):
+    print("Create random array of size " + str(n))
+    rand_arr = [x for x in range(1,n + 1)] # Create list of numbers between 1 and n
+    # rand_arr = [1,3,5,2,4,6]
+    random.shuffle(rand_arr) # Shuffle randomly generated list of numbers
+    count = 0 # Initialize the counter at 0
+    for i in range(n):
+        for j in range(i + 1, n): # Compare i with all numbers with a larger index in the list
+            if rand_arr[i] > rand_arr[j]:
+                count += 1
+    return count
+
+
+
+
+
+print(count_flip(2**16))

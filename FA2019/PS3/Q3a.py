73f8bb6fa09e59c31dc92d7687556ab32d0c5bcf
Michael <mike.rogers1272@gmail.com>
2019-09-17 15:45:24 -0600

    PS3 Final Commit

--- FA2019/PS3/Q3a.py
+++ FA2019/PS3/Q3a.py
@@ -2,12 +2,21 @@ def optimal_win_vals(n):
     win_vals = [1,2,4,8,16]
     remaining = n
     count = 0
-    while remaining > 0:
-        for i in win_vals[::-1]:
+    # while remaining != 0:
+    #     if win_vals[-1] <= remaining:
+    #         remaining -= win_vals[-1]
+    #         count += 1
+    #     else:
+    if win_vals[-1] <= remaining:
+        remaining = remaining - win_vals[-1]
+        count += 1
+    for i in range(0, len(win_vals)):
+        if win_vals[i] == remaining:
+            remaining = remaining - win_vals[i]
+        elif win_vals[i] > remaining:
             print(i)
-            if i <= remaining :
-                remaining -= i
-                count += 1
+            remaining = remaining - win_vals[i-1]
+            count += 1
     return count
 
-print(optimal_win_vals(100))
+print(optimal_win_vals(15))

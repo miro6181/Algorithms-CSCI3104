37ed2d53204f4227c40b02b26ce0b175c0f33cea
Michael <mike.rogers1272@gmail.com>
2019-11-22 09:37:31 -0700

    PS10b

--- FA2019/PS10/Rogers-Michael-PS10b-Q1.py
+++ FA2019/PS10/Rogers-Michael-PS10b-Q1.py
@@ -1,10 +1,19 @@
 import numpy as np
 
+# The alignStrings function is outputting the optimal cost of aligning two strings x and y.
+# The first thing I did was create a numpy array of zeros which is out empty matrix that we
+# will populate with the DP algorithm. I then put x in the first column and y in the first row.
+# Then I iterated through all of the empty elements fo the table to gain an optimal cost matrix.
+# The method of the DP algorithm was to choose the operation with the least cost. So a no-op was
+# the cheapest, and then it depended on user input. Once the algorithm is going it chooses based
+# on prior elements in the matrix to make it's decision. Unfortunatley, the algorithm seems to not
+# be working completely correctly, and I couldn't figure out how to fix it before the deadline.
+
 def alignStrings(x, y, cInsert, cDelete, cSub):
     S = np.zeros([len(y) + 1, len(x) + 1], dtype = int) # len(x) x len(y) Matrix of zeros
 
-    S[0] = [x for x in range(len(x) + 1)] # Put X in first row
-    for i in range(len(y) + 1): # Put Y in first column
+    S[0] = [x for x in range(len(x) + 1)] # Put x in first column
+    for i in range(len(y) + 1): # Put Y in first row
         S[i][0] = i
 
     for i in range(1, len(y) + 1):
@@ -16,11 +25,12 @@ def alignStrings(x, y, cInsert, cDelete, cSub):
     return S
 
 
-#
+# Not working / Incomplete
 # def extractAlignment(S, x, y, cInsert, cDelete, cSub):
 #     a = []
 #     if
 
+# Incomplete
 # def commonSubstrings(x, L, a):
 
 

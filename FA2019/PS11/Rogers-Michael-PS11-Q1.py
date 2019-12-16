71769b41119135b19d735f1c64c98b26a8e2d6f3
Michael <mike.rogers1272@gmail.com>
2019-12-15 20:28:38 -0700

    Final Exam

--- FA2019/PS11/Rogers-Michael-PS11-Q1.py
+++ FA2019/PS11/Rogers-Michael-PS11-Q1.py
@@ -1,47 +1,155 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import random
+#!/usr/bin/env python
+# coding: utf-8
 
-def hash_index(last_name, bucket):
+# # Problem Set 11 (Extra Credit)
+# ## Michael Rogers
+
+# ### Q1a.
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+import random as rd
+
+# h1(x)
+def h1_hash_index(name, location):
     hash = 0
-    for char in last_name:
-        hash += ord(char) - ord('A') + 1
-        hash = hash % bucket
+    for char in name:
+        #Hashing each character to it's appropriate location in the distribution.
+        hash += ord(char) - 65 + 1
+        hash = hash % location
     return hash
 
-last_name_hash = {}
+# h2(x)
+def h2_hash_index(name, location):
+    hash = 0
+    for char in name:
+        x_i = (ord(char) - 65 + 1)
+        a_i = rd.randint(0, location)
+        hash = hash + (x_i * a_i)
+        hash = hash % location
+    return hash
 
-bucket_size = 5987
+# Number of buckets
+l = 5987
 
-for i in range(0, bucket_size):
-    last_name_hash[i] = 0
+# Create name list with names randomly suffled
+names = []
 
-name_list = []
 with open("dist.all.last.txt") as file:
     for line in file:
-        last_name = line.split()[0]
-        names_list.append(last_name)
+        name = line.split()[0]
+        names.append(name)
 
-rand_indicies = random.sample(range(len(names_list)), len(names_list) // 2)
-last_name_list = [names_list[i] for i in indicies]
+rand_indicies = rd.sample(range(len(names)), len(names) // 2)
+names = [names[i] for i in rand_indicies]
 
-for name in last_name_list:
-    hash_value = hash_index(name,bucket_size)
-    last_name_hash[hash_value] += 1
+# Fill the hash1 dictionary
+hash1 = {x:0 for x in range(0, l)}
 
-print(last_name_hash)
+for name in names:
+    hash_value = h1_hash_index(name, l)
+    hash1[hash_value] += 1
 
-hash_lst = []
+hash1_lst = []
 
-for key in sorted(last_name_hash.keys()):
-    hash_lst.append(last_name_hash[key])
+for key in sorted(hash1.keys()):
+    hash1_lst.append(hash1[key])
 
-print(hash_lst)
-
-b = range(0,bucket_size)
-plt.bar(b, hash_lst)
-#plt.hist(hash_as_list, bins=b)
-plt.title("Histogram showing distribution of hash of last names")
-plt.xlabel("Bucket number")
-plt.ylabel("Size of bucket")
+# Plot hash1
+b = range(0,l)
+plt.bar(b, hash1_lst)
+plt.title("Last Name Distribution - Hash 1")
+plt.xlabel("Bucket")
+plt.ylabel("Number of Names in Hash Location")
 plt.show()
+
+# Fill the hash2 dictionary
+hash2 = {x:0 for x in range(0, l)}
+
+for name in names:
+    hash_value = h2_hash_index(name, l)
+    hash2[hash_value] += 1
+
+hash2_lst = []
+
+for key in sorted(hash2.keys()):
+    hash2_lst.append(hash2[key])
+
+# Plot hash2
+b = range(0,l)
+plt.bar(b, hash2_lst)
+plt.title("Last Name Distribution - Hash 2")
+plt.xlabel("Bucket")
+plt.ylabel("Number of Names in Hash Location")
+plt.show()
+
+
+# ### Q1c.
+
+# In[25]:
+
+
+max1bin = []
+
+# Function to find max in each bucket n 
+def max_in_bucket1(hash_lst):
+    maxnum = 0
+    for i in range(l):
+        if hash_lst[i] > maxnum:
+            maxnum = len(list(hash_lst.keys())[0:i])
+    max1bin.append(maxnum)   
+
+# Run above function for all names in list
+for name in names:
+    max_in_bucket1(hash1)
+
+if len(max1bin) > l:
+    max1bin = max1bin[0:l]
+print(len(max1bin))
+    
+# Plotting
+plt.plot([x for x in range(l)], max1bin)
+plt.title("Longest Chain vs. n - Hash 1")
+plt.xlabel("Element n")
+plt.ylabel("Chain length")
+plt.show()
+
+
+# ### Q1d.
+
+# In[27]:
+
+
+max2bin = []
+
+# Function to find max in each bucket n
+def max_in_bucket2(hash_lst):
+    maxnum = 0
+    for i in range(l):
+        if hash_lst[i] > maxnum:
+            maxnum = len(list(hash_lst.keys())[0:i])
+    max2bin.append(maxnum)
+
+# Run above function for all names in list
+for name in names:
+    max_in_bucket2(hash2)
+    
+if len(max2bin) > l:
+    max2bin = max2bin[0:l]
+print(len(max2bin))
+
+# Plotting
+plt.plot([x for x in range(l)], max2bin)
+plt.title("Longest Chain vs. n - Hash 2")
+plt.xlabel("Element n")
+plt.ylabel("Chain length")
+plt.show()
+
+
+# In[ ]:
+
+
+
+
